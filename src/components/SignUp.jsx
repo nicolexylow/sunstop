fae63e9dbbddf8df80932fd7efa3f891949f7ec7
Nicole Low <nicolexylow@gmail.com>
2024-10-13 19:20:29 +1100

    weather API integration and page navigations

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,4 +1,5 @@
 import styles from '../scss/modules/SignUp.module.scss';
+import '../scss/global.scss'
 import { useNavigate } from 'react-router-dom'
 
 function SignUp() {
@@ -9,17 +10,23 @@ function SignUp() {
         navigate('/verify');
     }
 
+    const handleBack = () => {
+        navigate('/');
+    }
+
     return (
         <>
-        <button className={styles['back-button']}>Back</button>
-        <div className={styles['center-container']}>
-            <div className={styles['content-container']}>
-                <h1 className={styles['heading']}>Sign Up</h1>
-                <form onSubmit={handleSubmit}>
-                    <label for="details">Enter details:</label><br />
-                    <input type="text" placeholder='Email or Phone' className={styles['input-field']}/><br />
-                    <input type="submit" value="Submit" />
-                </form>
+        <div className="main-container">
+            <button className={styles['back-button']} onClick={handleBack}>Back</button>
+            <div className={styles['center-container']}>
+                <div className={styles['content-container']}>
+                    <h1 className={styles['heading']}>Sign Up</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label for="details">Enter details:</label><br />
+                        <input type="text" placeholder='Email or Phone' className={styles['input-field']}/><br />
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
             </div>
         </div>
         </>
