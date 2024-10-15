dec7d21e84fdef1d600e3fdb4ae80a22192c8b46
unknown <bwal9030@gmail.com>
2024-10-15 15:49:21 +1100

    fully working dispense points page

--- src/components/DispensePoints.jsx
+++ src/components/DispensePoints.jsx
@@ -2,18 +2,99 @@ import styles from '../scss/modules/DispensePoints.module.scss';
 import { useNavigate } from 'react-router-dom';
 import { RollingNumber } from '@layflags/rolling-number';
 
-function InitTrackerMilestones() {
-    return RenderTrackerMilestone();
+/* 
+###############
+## Variables ##
+###############
+*/
+
+/* USER POINTS */
+// There are four milestones, reached at each multiple of 200 up to
+// 800 points
+const userPoints = 320;
+const userPointsMax = 1000;
+// How many points has user gained?
+const userPointsGained = 10;
+// New points total
+let userPointsNew = 0;
+
+// Total width of line container in html
+const lineWidthHTML = 1000;
+// Tracker line widthS
+let trackLineWidth = userPoints;
+
+
+function renderScoreHead() {   
+    userPointsNew = userPoints + userPointsGained;
+
+    return (
+        <>
+            <div id={styles['points-total-container']}>                    
+                <layflags-rolling-number className={styles['points-total-dial']}>
+                    {userPointsNew}
+                </layflags-rolling-number>
+            </div>
+            <h1 class={styles['points-gained']}>+{userPointsGained}</h1>
+        </>
+    )
 }
-function RenderTrackerMilestone() {
+
+// Milestone tracker stuff
+function InitTrackerMilestones() {
+    // How many milestones has user unlocked?
+    // This season's milestones
+    const milestoneArr = [
+        {item: 'test1', img: 'laundry'},
+        {item: 'test2', img: 'laundry'},
+        {item: 'test3', img: 'laundry'},
+        {item: 'test4', img: 'laundry'}
+    ];
+
+    const milestoneArrNo = milestoneArr.length;
+    console.log(milestoneArrNo);
+    const milestoneWidth = lineWidthHTML / milestoneArrNo;
+
+    // HTML array
+    const renderedMilestoneArr = [];
+
+    // Pushing HTML elements to array
+    for (let i = 0; i < milestoneArrNo; i++) {
+        const iPoints = (i + 1) * 200;
+
+        // If we've reached the user has just reached the points 
+        // threshold, and it hasn't already been reached, push the
+        // active milestone HTML
+        if ( (iPoints / userPointsNew) <= 1 && (iPoints / userPoints) >= 1) {
+            renderedMilestoneArr.push(RenderTrackerMilestone(milestoneWidth, `${i} ${milestoneArr[i].item}`, milestoneArr[i].img, 'active'));
+        } 
+        // If user reached milestone previously, push previously 
+        // redeemed milestone HTML
+        else if ( (iPoints / userPoints) <= 1 ) {
+            renderedMilestoneArr.push(RenderTrackerMilestone(milestoneWidth, `${i} ${milestoneArr[i].item}`, milestoneArr[i].img, 'active-past'));
+        } 
+        // Otherwise, push unredeemed milestone HTML
+        else {
+            renderedMilestoneArr.push(RenderTrackerMilestone(milestoneWidth, `${i} ${milestoneArr[i].item}`, milestoneArr[i].img, 'default'));
+        }
+    };
+
+    return ( 
+        <>
+            {renderedMilestoneArr}; 
+        </>
+    );
+}
+function RenderTrackerMilestone(lineWidthHTML, item, img, state) {
 
     return (
         <>
             {/* Wrap milestone elements */}
-            <div className={styles['milestone']}>
+            <div className={`${styles['milestone']} ${styles[state]}`} key={item} style={{ width: lineWidthHTML }}>
                 {/* Icon w/ img */}
-                <div className={styles['milestone-icon']}>
-
+                <div className={styles['milestone-icon-container']}>
+                    <span className={`material-symbols-rounded`}>
+                        laundry
+                    </span>
                 </div>
                 {/* Line container, has hacky endpoint */}
                 <div className={styles['milestone-line-container']}>
@@ -21,6 +102,25 @@ function RenderTrackerMilestone() {
                     <div className={styles['milestone-line']}> </div>
                 </div>
             </div>
+        </>
+    )
+}
+
+// Update user points on head and tracker
+function initUserPointsUpdate() {
+
+}
+function renderUserPointsUpdate() {
+}
+function renderUserPointsLine() {
+    // Calculate line width based on user points/points max
+    // For now, points are the same as max line width
+    trackLineWidth = userPointsNew;
+
+    return (
+        <>
+        <div id={styles['tracker-baseline']} className={styles['active']} style={{ width: trackLineWidth }}>
+        </div>
         </>
     )
 }
@@ -46,7 +146,7 @@ function DispensePoints() {
 
     return (
         <>
-\        {/* Wrap content */}
+        {/* Wrap content */}
         <div id='wrapper'>
             {/* Header: Back btn, indexical title, logout */}
             <header>
@@ -60,12 +160,10 @@ function DispensePoints() {
                 {/* Main head: Points counter */}
                 <div id={styles['main-head']}>
                     <h3>Your score is now</h3>
+
                     {/* Points total and rolling text dial to signify new points */}
                     <div id={styles['points-container']}>
-                        <h1 id={styles['points-total']}>10</h1>
-                        <div className={styles['points-dial-container']}>                    
-                            <layflags-rolling-number className={styles['points-dial']}>123</layflags-rolling-number>
-                        </div>
+                        {renderScoreHead()}
                     </div>
                 </div>
                 
@@ -74,13 +172,15 @@ function DispensePoints() {
                     {/* Our milestones sit here */}
                     <div id={styles['tracker-milestones-container']}>
                         {/* Insert row of milestones */}
-                        {/* Temporarily, insert two */}
-                        {InitTrackerMilestones()}
                         {InitTrackerMilestones()}
                     </div>
-                    {/* Baseline */}
-                    <div id={styles['tracker-baseline']}>
+                    <div id={styles['tracker-baseline-container']}>
+                        {/* Baseline base */}
+                        <div id={styles['tracker-baseline']}>
 
+                        </div>
+                        {/* Baseline active */}
+                        {renderUserPointsLine()}
                     </div>
                 </div>
                 {/* Ben put your code here */}
