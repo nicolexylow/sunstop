b8850a51c8093cd3f779713d3852ca1cddb42f94
Nicole Low <nicolexylow@gmail.com>
2024-10-26 17:19:01 +1100

    dispense a little more page

--- src/components/Dispense3LilMore.jsx
+++ src/components/Dispense3LilMore.jsx
@@ -1,111 +1,26 @@
+import PageTemplate from "./PageTemplate";
+import sunIcon from '../assets/sun-icon.png';
 import styles from '../scss/modules/DispenseLilMore.module.scss';
-import { useLocation, useNavigate } from 'react-router-dom';
-import PageTemplate from './PageTemplate';
 
-// Wizard of Oz control over sunscreen application stage
-import Reactotron from "reactotron-react-js"
-Reactotron.onCustomCommand({
-    command: "test2",
-    handler: () => console.log("Test wizardofOz script"),  // Optional settings
-    title: "Next dispense", // This shows on the button
-    description: "Transition UI to the next sunscreen application body part", // This shows below the button
-});
-
-// Init figure stuff: function for overall html 
-function InitFigure(props) {
-    return (
-        <>
-            <div id={styles['main-left-figure']}>
-                <div id={styles['figure-img']}>
-                    <RenderFigureImg img={props.outfit.img}/>
-                </div>
-                <div id={styles['figure-labels-list']}>
-                    <RenderFigureLabels body={props.outfit.body}/>
-                </div>
-            </div>
-        </>
-    );
-};
-// Grab image 
-function RenderFigureImg(props) {
-
-}
-// Load from which body part tags we are cycling through
-function RenderFigureLabels(props) {
-    // Extract variables for body parts
-    const labelsArr = props.body;
-    const labelsNo = labelsArr.length;
-    console.log(labelsArr);
-    console.log(labelsNo);
-
-    // Array of label html
-    const labelsDOM = [];
-
-    // Loop through the amount of body parts specified
-    for (let i=0; i < labelsNo; i++) {
-        // Push body part label to div array
-        labelsDOM.push(FigureLabel(i, labelsArr[i]))
-    };
-    console.log(labelsDOM);
-
-    // Return label html array
-    return (
-        <>
-            {labelsDOM}
-        </>
-    );
-};
-// HTML for body part label
-function FigureLabel(num, bodyPart) {
-    // Return our body part label, number and name
-    return  <div className={styles['label']} id={num}> 
-                <a>{num}.</a> 
-                <p>{bodyPart}</p> 
-            </div>;
-};
-
-function DispenseActive() {
-    // Extract variables for body parts from Dispense page using useLocation
-    const location = useLocation();
-    const outfitPull = location.state.outfitPush;
-    console.log(outfitPull);
-
-    // Navigation here
-    const navigate = useNavigate();
-        // Back btn
-        // We need another page instead of going back to dispense. Maybe dashboard page?
-        // If we allow the user to go back all the time, they can dispense infinite sunscreen
-        // and exploit our free sunscreen functions.
-        const handleBkTap = () => {
-            navigate('/dispense0')
-        };
-        // Log out btn
-        const handleLogOutTap = () => {
-            navigate('/')
-        };
-    
+function Dispense3LilMore() {
     return (
         <>
         <PageTemplate>
-            {/* Wrap content */}
-            <main id={styles['main']}>
-                {/* Left col: Body diagram, labels */}
-                <InitFigure outfit={outfitPull}/>
-                {/* Right col: Text, hand fig */}
-                <div id={styles['main-right']}>
-                    <div id={styles['right-title']}>
-                        <h1>Dispensing sunscreen for</h1>
-                        <p id={styles['dispense-area']}>Face</p>
-                        <h2>Place hand under</h2>
-                    </div>
-                    <button className="btn-scnd" id={styles['btn-skip']}>Skip</button>
+            <div className="main-container">
+                <div className={styles['content-container']}>
+                    <img src={sunIcon} alt="SunStop Icon" className={styles['sun-icon']} />
+                    <h1 className={styles['heading']}>Thank you for being <span className={styles['highlight-text']}>SunSafe</span>!</h1>
                 </div>
-                {/* Ben put your code here */}
-            </main>
+
+                <div className={styles['button-container']}>
+                    <button className={`${styles['more-button']} ${styles['button']}`}>I need a little more sunscreen</button>
+                    <button className={`${styles['finish-button']} ${styles['button']}`}>Finish</button>
+                </div>
+                
+            </div>
         </PageTemplate>
         </>
     );
-};
+}
 
-
-export default DispenseActive
+export default Dispense3LilMore;
